1a4bf95973cd0895011b58b6f792857f4ab3bd2d
천국 <71075510+jsy526@users.noreply.github.com>
2021-09-12 19:52:39 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,1 @@
-const { CommandInteraction } = require('discord.js')
 
-module.exports = {
-	name: "핑",
-	description: '퐁',
-	/**
-	 * @param { CommandInteraction } Interaction
-	 */
-
-	async execute(interaction) {
-		await interaction.reply(`pong!`)
-	}
-}
