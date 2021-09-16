04f96ed2fa2e8595f8c94e9a76641135b8008f65
천국 <71075510+jsy526@users.noreply.github.com>
2021-09-16 19:18:19 +0900

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,15 +1,15 @@
 const { CommandInteraction } = require('discord.js')
 
 module.exports = {
-	name: "verify",
-	description: '인증',
+	name: "인증",
+	description: 'verify',
 	/**
 	 * @param { CommandInteraction } Interaction
 	 */
 
 	async execute(interaction) {        
-		await interaction.reply(`성공적으로 지급 되었습니다!`);
-		
+		await interaction.reply({ content: '성공적으로 지급 되었습니다!', ephemeral: true });
         interaction.member.roles.add("858870021265686569");
+		
 	}
 }
