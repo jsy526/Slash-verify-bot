3207d65015f0afe157218b3e36b4c60b8a1a385b
천국 <71075510+jsy526@users.noreply.github.com>
2021-10-17 19:41:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client,Intents, Collection} = require('discord.js');
+const {Client,Intents, Collection,MessageEmbed} = require('discord.js');
 const client = new Client({intents:[Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MEMBERS]})
 const {token} = require('./config.json');
 const fs = require('fs');
@@ -11,6 +11,34 @@ for(const file of commandFiles) {
 	client.commands.set(command.name, command);
 	data.push({name: command.name, description: command.description, options:command.options});
 }
+
+client.on('messageUpdate', async(oldMessage, newMessage) => {
+    if(oldMessage.content === newMessage.content) return 
+    if(oldMessage.author.bot) return;
+    const img = oldMessage.author.avatar ? `https://cdn.discordapp.com/avatars/${oldMessage.author.id}/${oldMessage.author.avatar}.webp?size=256` : undefined;
+    const embed = new MessageEmbed()
+    .setTitle(`${oldMessage.author.tag}님이 메시지를 수정했습니다.`)
+    .setColor('#FFFF')
+    .addField('수정 전 메시지:', oldMessage.content)
+    .addField('수정 후 메시지:', newMessage.content)
+    .setFooter(oldMessage.author.tag, img)
+    .setTimestamp()
+    client.channels.cache.get('898819684089081876').send({ embeds: [embed] })
+  })
+  
+  
+  
+  client.on('messageDelete', async message => {
+  if(message.author.bot) return
+  const img = message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256` : undefined;
+  const embed = new MessageEmbed() 
+  .setTitle(`${message.author.tag} 님이 메시지를 삭제했습니다.`)
+  .setColor('#FFFF')
+  .addField('삭제된 메시지:', message.content)
+  .setFooter(message.author.tag, img)
+  .setTimestamp()
+  client.channels.cache.get('898819684089081876').send({ embeds: [embed] })
+  });
 
 client.once('ready', async () => {
 	console.log(`Logged In As ${client.user.tag}`)
