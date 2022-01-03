0ab4e3688e29148c1c9898e2a66ca8efcf5e47ff
천국 <71075510+jsy526@users.noreply.github.com>
2022-01-03 17:50:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ client.on('messageUpdate', async(oldMessage, newMessage) => {
     if(oldMessage.author.bot) return;
     const img = oldMessage.author.avatar ? `https://cdn.discordapp.com/avatars/${oldMessage.author.id}/${oldMessage.author.avatar}.webp?size=256` : undefined;
     const embed = new MessageEmbed()
-    .setTitle(`${oldMessage.author.tag}님이 메시지를 수정했습니다.`)
+    .setTitle(`**${oldMessage.author.tag}**님이 메시지를 수정했습니다.`)
     .setColor('#FFFF')
     .addField('수정 전 메시지:', oldMessage.content)
     .addField('수정 후 메시지:', newMessage.content)
     .setFooter(oldMessage.author.tag, img)
     .setTimestamp()
-    client.channels.cache.get('899245273413451807').send({ embeds: [embed] })
+    client.channels.cache.get('927482738590744596').send({ embeds: [embed] })
   })
   
   
@@ -32,17 +32,52 @@ client.on('messageUpdate', async(oldMessage, newMessage) => {
   if(message.author.bot) return
   const img = message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256` : undefined;
   const embed = new MessageEmbed() 
-  .setTitle(`${message.author.tag} 님이 메시지를 삭제했습니다.`)
+  .setTitle(`**${message.author.tag}**님이 메시지를 삭제했습니다.`)
   .setColor('#FFFF')
   .addField('삭제된 메시지:', message.content)
   .setFooter(message.author.tag, img)
   .setTimestamp()
-  client.channels.cache.get('899245273413451807').send({ embeds: [embed] })
+  client.channels.cache.get('927389830780968980').send({ embeds: [embed] })
   });
+
+ // client.on('message', message =>{
+    //if(message.channel.id === '927482761718169620' && message.member.hasPermission('ADMINISTRATOR') && message.author.id === ('825604702011785258')){
+        
+     //   message.react("1️⃣");
+      //  message.react("2️⃣");
+      //  message.react("3️⃣");
+      //  
+       // const Filter_1 = (reaction) => reaction.emoji.name === "1️⃣";
+       // const Filter_2 = (reaction) => reaction.emoji.name === "2️⃣";
+       // const Filter_3 = (reaction) => reaction.emoji.name === "3️⃣";
+
+      //  const role_1 = message.createReactionCollector(Filter_1, {time: 900000, dispose: true});
+       // const role_2 = message.createReactionCollector(Filter_2, {time: 900000, dispose: true});
+      //  const role_3 = message.createReactionCollector(Filter_3, {time: 900000, dispose: true});
+
+     //   role_1.on("collect", async (reaction, user) => {
+            await reaction.message.guild.members.cache.get(user.id).roles.add('903529970708660234')
+      //  });
+
+      //  role_2.on("collect", async (reaction, user) => {
+         //   await reaction.message.guild.members.cache.get(user.id).roles.add('903529979319570472')
+      //  });
+
+       // role_3.on("collect", async (reaction, user) => {
+           // await reaction.message.guild.members.cache.get(user.id).roles.add('903529982633070613')
+        //});
+   // }
+//})
+
+//client.on('guildMemberAdd', guildMember =>{
+    //guildMember.setNickname(`신입_${guildMember.user.username}`) 
+   // guildMember.addRole('900711319714533406') //들어오면 역할주는거
+    //guildMember.guild.channels.cache.get('903590620709408769').send(`<@${guildMember.user.id}>${message.guild.name}에 오신걸 환영해요!`)
+  //});
 
 client.once('ready', async () => {
 	console.log(`Logged In As ${client.user.tag}`)
-	await client.guilds.cache.get('899245273413451807')?.commands.set(data)
+	await client.guilds.cache.get('921594738338062366')?.commands.set(data)
 });
 
 const Dokdo = require('dokdo')
@@ -50,7 +85,7 @@ const Dokdo = require('dokdo')
 const DokdoHandler = new Dokdo(client, { aliases: ['dokdo', 'dok', '독도'], prefix: 's'})
 
 client.on('messageCreate', message => {
-    DokdoHandler.run(message)
+  DokdoHandler.run(message)
 })
 
 client.on('interactionCreate', async interaction => {
